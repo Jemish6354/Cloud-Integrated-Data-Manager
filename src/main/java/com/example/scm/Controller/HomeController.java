7e242ef41723e7e1dbd5a9d4c37446248adfaf8b
jemish6354 <jglathiya60575@gmail.com>
2025-10-17 08:50:37 +0530

    second commit

--- src/main/java/com/example/scm/Controller/HomeController.java
+++ src/main/java/com/example/scm/Controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 
-import com.example.scm.Dao.UserRepository;
+
 import com.example.scm.Entities.User;
 import com.example.scm.Helper.Message;
 import com.example.scm.Helper.MessageType;
@@ -21,7 +21,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
+
 
 
 
